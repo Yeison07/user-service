9b88268d085442b0a0be44c204b84f57887a79e2
Yeison07 <yeisona07@hotmail.com>
2023-04-02 18:06:03 -0500

    Unit testing add unit testing to rest user controller with junit and mockito

---
+++ src/main/java/co/ufps/edu/userservice/infrastructure/driver_adapters/jpa_repository/ProjectIdsData.java
@@ -0,0 +1,29 @@
+package co.ufps.edu.userservice.infrastructure.driver_adapters.jpa_repository;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.NotFound;
+import org.hibernate.annotations.NotFoundAction;
+
+@Entity
+@Table(name = "projectId")
+public class ProjectIdsData {
+    @Id
+    private Long projectsId;
+
+    public ProjectIdsData() {
+    }
+
+    public ProjectIdsData(Long projectsId) {
+        this.projectsId = projectsId;
+    }
+
+    public Long getProjectsId() {
+        return projectsId;
+    }
+
+    public void setProjectsId(Long projectsId) {
+        this.projectsId = projectsId;
+    }
+}
---
+++ src/test/java/co/ufps/edu/userservice/infrastructure/entry_points/UserControllerTest.java
@@ -0,0 +1,55 @@
+package co.ufps.edu.userservice.infrastructure.entry_points;
+
+import co.ufps.edu.userservice.domain.model.User;
+import co.ufps.edu.userservice.domain.useCase.UserUseCase;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.io.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UserControllerTest  {
+
+    @InjectMocks
+    UserController userController;
+
+    @Mock
+    UserUseCase useCase;
+
+    @Test
+    public void testAddUser(){
+        var request=new MockHttpServletRequest();
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+        User user= new User("test@hotmail.com","123","nameTest","lastNameTest");
+        ResponseEntity<Object> responseEntity= userController.registerNewUser(user);
+        assert(responseEntity.getStatusCode().isSameCodeAs(HttpStatus.CREATED));
+    }
+
+    @Test
+    public void testFindAll(){
+        User user1= new User("test1@hotmail.com","123","nameTest1","lastNameTest1");
+        User user2= new User("test2@hotmail.com","231","nameTest2","lastNameTest2");
+        User user3= new User("test3@hotmail.com","132","nameTest3","lastNameTest3");
+
+        List<User>users= Arrays.asList(user1,user2,user3);
+        when(useCase.findAllUsers()).thenReturn(users);
+        var result= userController.getAllUser();
+        assertEquals(result.getBody(),users);
+
+    }
+}
